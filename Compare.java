1604f205cd61c1f0127369e02fc98baafe95a11c
Dhwanan <36649623+dhwanan@users.noreply.github.com>
2018-04-10 22:16:53 -0700

    Add files via upload

---
+++ Compare.java
@@ -0,0 +1,24 @@
+
+public class Compare {
+
+	public static void main(String[] args) {
+		compare();
+		
+	}
+		public static void compare(){
+			
+	int x=2;
+	int y=3;
+	
+	if(x==y)
+	{
+	System.out.println("x equal to y");	
+	}
+
+	else
+	{
+		System.out.println("x not equal to y");
+	}
+	}
+
+}
